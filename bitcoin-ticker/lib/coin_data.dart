87deb8acbb20280567d4fedd24b6401b77db5589
Med Redha <redha.dirac@gmail.com>
2020-04-11 22:46:04 +0100

    initial commit yeay! :construction_worker:

---
+++ bitcoin-ticker/lib/coin_data.dart
@@ -0,0 +1,52 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const List<String> currenciesList = [
+  'ARS',
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+  'XRP',
+];
+
+class CoinData {
+  CoinData(this.url);
+
+  final String url;
+
+  Future getCoinData() async {
+    http.Response response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
