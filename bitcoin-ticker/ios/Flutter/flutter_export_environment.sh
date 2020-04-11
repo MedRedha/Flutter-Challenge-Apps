f7eced2c5a41380ba2e2e8a9d7a8fccb988c9f64
Med Redha <redha.dirac@gmail.com>
2020-04-11 22:41:37 +0100

    initial commit yeay! :apple:

---
+++ bitcoin-ticker/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/MedRedha/Desktop/Dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/MedRedha/Desktop/Dev/Flutter-Challenge-Apps/flutter-appbrewery-master/bitcoin-ticker"
+export "FLUTTER_TARGET=/Users/MedRedha/Desktop/Dev/Flutter-Challenge-Apps/flutter-appbrewery-master/bitcoin-ticker/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/MedRedha/Desktop/Dev/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
